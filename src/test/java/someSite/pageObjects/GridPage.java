077612b2a2a908f24b8b4755b1af89cdde3c4300
Milevskiy <ajleksandr991@gmail.com>
2023-03-26 15:54:50 +0300

    Refactoring

--- src/test/java/someSite/pageObjects/GridPage.java
+++ src/test/java/someSite/pageObjects/GridPage.java
@@ -9,6 +9,7 @@ import static com.codeborne.selenide.Selenide.$x;
 public class GridPage {
     private static final SelenideElement ELEMENT_GRID_AND_TITLE = $x("//div[@class='app-title' and text()='Grid & chart']");
     private static final SelenideElement ELEMENT_GRID_CELL = $x("//div[@role='gridcell']");
+    private static final SelenideElement ELEMENT_GRID_TABLE = $x("//div[contains(@class, 'fullscreen')]");
     private static final SelenideElement ELEMENT_CHART_LINES = $x("//span[@class='chart-settings__title chart-mobile-title']");
 
     ElementsCollection allCellsInGrid = $$x("//div[@role='gridcell']");
